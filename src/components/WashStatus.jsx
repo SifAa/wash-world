50870e7d8d9798acef9ddfb2544abef692947172
Sif <sif_aagaard_svendsen@hotmail.com>
2022-04-02 15:09:56 +0200

    some corrections, timer is a bit funky though

--- src/components/WashStatus.jsx
+++ src/components/WashStatus.jsx
@@ -5,16 +5,75 @@ import info from "../info";
 export default function WashStatus(data) {
   useEffect(() => {
     axios
-      .get(info.backendUrl + "/" + data.locationID + "/start/" + data.programID) // this link does not work also when I try in the browser, have I miss understood how the link is supposed to work?
+      .post(
+        info.backendUrl + "/" + data.locationID + "/start/" + data.programID
+      )
       .then((result) => {
-        console.log(result.data);
-        //data.setProducts(result.data.response.products);
+        //console.log(result.data.response);
+        data.setProgram(result.data.response);
       });
   }, []);
 
+  //var counter = document.getElementById("cycleTimer");
+
+  var duration = data.program.estimated_duration;
+
+  if (duration) {
+    var durationArray = duration.split(":");
+    var minutes = durationArray[0];
+    var seconds = checkSecond(durationArray[1] - 1);
+    document.getElementById("timer").innerHTML = minutes + ":" + seconds;
+    startTimer();
+  }
+
+  function startTimer() {
+    var presentTime = document.getElementById("timer").innerHTML;
+    var timeArray = presentTime.split(/[:]+/);
+    var m = timeArray[0];
+    var s = checkSecond(timeArray[1] - 1);
+    if (s == 59) {
+      m = m - 1;
+    }
+    if (m < 0) {
+      return;
+    }
+
+    document.getElementById("timer").innerHTML = m + ":" + s;
+    setTimeout(startTimer, 1000);
+  }
+
+  function checkSecond(sec) {
+    if (sec < 10 && sec >= 0) {
+      sec = "0" + sec;
+    } // add zero in front of numbers < 10
+    if (sec < 0) {
+      sec = "59";
+    }
+    return sec;
+  }
+
   return (
-    <div>
-      <h2>Time</h2>
+    <div className="program-container">
+      {Object.keys(data.program).length === 0 && (
+        <div className="lds-default">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      )}
+      <h2>{data.program.program}</h2>
+      <div id="cycleTimer">
+        <span id="timer"></span>
+      </div>
     </div>
   );
 }
